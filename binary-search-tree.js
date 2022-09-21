c0957c9dedba0a27be019ad91cfed5862c800fa1
Raytene <raytenehan@gmail.com>
2022-09-21 15:25:07 -0700

    initial commit

---
+++ binary-search-tree.js
@@ -0,0 +1,89 @@
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** insert(val): Insert a new node into the BST with value val.
+   * Returns the tree. Uses iteration. */
+
+  insert(val) {
+
+  }
+
+  /** insertRecursively(val): Insert a new node into the BST with value val.
+   * Returns the tree. Uses recursion. */
+
+  insertRecursively(val) {
+
+  }
+
+  /** find(val): Search the tree for a node with value val.
+   * Return the node, if found; else undefined. Uses iteration. */
+
+  find(val) {
+
+  }
+
+  /** findRecursively(val): Search the tree for a node with value val.
+   * Return the node, if found; else undefined. Uses recursion. */
+
+  findRecursively(val) {
+
+  }
+
+  /** dfsPreOrder(): Traverse the array using pre-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPreOrder() {
+
+  }
+
+  /** dfsInOrder(): Traverse the array using in-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsInOrder() {
+
+  }
+
+  /** dfsPostOrder(): Traverse the array using post-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPostOrder() {
+
+  }
+
+  /** bfs(): Traverse the array using BFS.
+   * Return an array of visited nodes. */
+
+  bfs() {
+
+  }
+
+  /** findSuccessorNode(): Find the node with the next largest value.
+   * Return successor node or undefined if not found. */
+
+  findSuccessorNode(node) {
+
+  }
+
+  /** Further Study!
+   * remove(val): Removes a node in the BST with the value val.
+   * Returns the removed node. */
+
+  remove(val) {
+
+  }
+}
+
+module.exports = {
+  BinarySearchTree,
+  Node,
+};
